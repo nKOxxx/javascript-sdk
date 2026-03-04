0100eed82ddad56a00d40893d3034550fb910d3f
nKOxxx <nikolaxxx@protonmail.com>
2026-03-04 18:22:23 +0400

    chore: setup prettier for consistent code formatting
    - Add prettier.config.js with sensible defaults
    - Add .prettierignore to exclude build artifacts
    - Add npm scripts: format and format:check
    - Add prettier@^3.2.0 to devDependencies
    
    Closes #11

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/en/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+};
+
+export default config;
